fccb3db7eff4393b5d8826d869bbcb042edc89aa
AIME-LOIC <kingaime132@gmail.com>
2026-06-14 14:53:37 +0200

    fix: update import path for sequelize configuration in notification model

--- src/database/models/notification.js
+++ src/database/models/notification.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../../config/db.js';
+import db from '../../config/local.db.js';
 
 const Notification = sequelize.define('Notification', {
   id:             { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
